78dfbbc267649bf66ef6a0dc2b9fbe461ae74dd9
Unovamata <badelgado@uned.cr>
2023-05-29 15:57:12 -0600

    Readability and Script Renaming
    • The "CrapTextureManagement" script now is more readable with descriptive comments. Also, removed a lot of unused variables in the code for debug purposes.
    • The original "SkeletonSO" script has been renamed to "TexturesSO" as it makes more sense conceptually.
    • A new script "SkeletonSO" has been created to handle the relations between the skeleton and parts of the object.

--- Unity Project/Texture Colorizer/Assets/CrAPTextureManagement.cs
+++ Unity Project/Texture Colorizer/Assets/CrAPTextureManagement.cs
@@ -4,60 +4,54 @@ using UnityEditor;
 using UnityEngine;
 
 public class CrAPTextureManagement : MonoBehaviour{
-    public static CrAPTextureManagement Instance;
-
-    private void Awake() {
-        Instance = this;
-    }
-
     //Data;
-    public SkeletonSO skeleton, skeletonToReference;
+    public TexturesSO skeleton, skeletonToReference;
     public Texture2D skeletonData;
-    [HideInInspector] public Texture2D temporalTexture;
+    [HideInInspector] public Texture2D newTextureToManage;
 
     //Unity Editor;
     Material material;
     MeshRenderer mesh;
-    [HideInInspector] public int x, y, w, h;
-    Limb limb;
 
-    //Callers;
-    [SerializeField] string limbName = "Head";
 
-    // Start is called before the first frame update
+    ////////////////////////////////////////////////////////////////////
+
+    
     void Start() {
+        //Extracting the material;
         mesh = GetComponent<MeshRenderer>();
         material = mesh.material;
-        //LoadSkeletonData("SkeletonData", skeleton);
-
-        //Texture to edit;
-        temporalTexture = CloneTexture(skeletonData);
-
-        try {
-            limb = skeleton.CallLimb(limbName);
-            Vector4 coordinates = limb.GetCoordinates();
-            x = (int)coordinates.x;
-            y = (int)coordinates.y;
-            w = (int)coordinates.z;
-            h = (int)coordinates.w;
-        } catch { 
-            Debug.LogError("There's no limb with the name: " + limbName);
-        }
         
-        material.mainTexture = temporalTexture;
+        //Creating a new texture based in the original one;
+        newTextureToManage = CloneTexture(skeletonData);
+        
+        //Assigning the new texture;
+        material.mainTexture = newTextureToManage;
         mesh.material = material;
+
+        //Circumvents the infinite material swapping provoked by UniSWF;
         enabled = false;
-        //AssetDatabase.CreateAsset(mesh, "Mesh");
-        //AssetDatabase.SaveAssets();
     }
 
-    // Update is called once per frame
+    //Clone a reference texture;
+    private Texture2D CloneTexture(Texture2D original) {
+        Texture2D clone = new Texture2D(original.width, original.height);
+        clone.SetPixels(original.GetPixels());
+        clone.Apply();
+        return clone;
+    }
+
+
+    ////////////////////////////////////////////////////////////////////
+
+
     void Update(){
-        material.mainTexture = temporalTexture;
+        material.mainTexture = newTextureToManage;
         mesh.material = material;
     }
-
-    public static void LoadSkeletonData(string route, SkeletonSO skeleton) {
+    
+    //Loads data into a SkeletonSO object;
+    public static void LoadSkeletonData(string route, TexturesSO skeleton) {
         //Sprite has to be located in the "Resources" folder;
         UnityEngine.Sprite[] sprites = Resources.LoadAll<UnityEngine.Sprite>(route);
         string path = CreateFolder(skeleton.name);
@@ -93,23 +87,24 @@ public class CrAPTextureManagement : MonoBehaviour{
             texture.SetPixels(pixels);
             texture.Apply();
             
-            // Saving the texture as an asset
+            // Saving the texture as an asset for later reference;
             string texturePath = path + "/" + sprite.name + ".asset"; // Specify the desired path and filename
             AssetDatabase.CreateAsset(texture, texturePath);
             AssetDatabase.SaveAssets();
             AssetDatabase.Refresh();
 
-            limb.SetTexture(AssetDatabase.LoadAssetAtPath<Texture2D>(texturePath));
-
             //Saving the data;
+            limb.SetTexture(AssetDatabase.LoadAssetAtPath<Texture2D>(texturePath));
             skeleton.AddLimb(limb);
         }
     }
 
+    //Create a folder to hold assets;
     public static string CreateFolder(string skeletonName) {
         string baseRoute = "Assets/Textures";
         string folderPath = baseRoute + "/" + skeletonName;
 
+        //If the folder has a valid path and name, create it;
         if (!AssetDatabase.IsValidFolder(folderPath)) {
             AssetDatabase.CreateFolder(baseRoute, skeletonName);
             Debug.Log("Folder Created: " + skeletonName);
@@ -118,67 +113,73 @@ public class CrAPTextureManagement : MonoBehaviour{
         return folderPath;
     }
 
-    private Texture2D CloneTexture(Texture2D original) {
-        Texture2D clone = new Texture2D(original.width, original.height);
-        clone.SetPixels(original.GetPixels());
-        clone.Apply();
-        return clone;
+    //Remove a specific texture at a position;
+    public void ClearTextureAt(int x, int y, int w, int h, Texture2D texture) {
+        //Get transparent color;
+        Color32[] pixels = FillArrayColor(Color.clear, w, h);
+
+        //Save the converted pixels;
+        texture.SetPixels32(x, y, w, h, pixels);
+        texture.Apply();
     }
 
-    public void ClearTextureAt(int x, int y, int w, int h) {
-        Color[] c = FillArrayColor(Color.clear, w, h);
-        temporalTexture.SetPixels(x, y, w, h, c);
-        temporalTexture.Apply();
-    }
 
-    public void PasteTexture(Limb limb, Texture2D skeletonTexture) {
+    //Paste a specific limb texture to the texture sheet;
+    public void PasteTexture(Limb limb, Texture2D skeletonTexture, TexturesSO reference) {
         Vector4 coordinates = limb.GetCoordinates();
-    int x = (int)coordinates.x, y = (int)coordinates.y, w = (int)coordinates.z, h = (int)coordinates.w;
+        int x = (int)coordinates.x, y = (int)coordinates.y, w = (int)coordinates.z, h = (int)coordinates.w;
 
-    Texture2D sourceTexture = skeletonToReference.CallLimb(limb.GetName()).GetTexture();
+        //Extracting the texture to paste from the limb referenced;
+        Texture2D pasteTexture = reference.CallLimb(limb.GetName()).GetTexture();
+        int sourceWidth = pasteTexture.width;
+        int sourceHeight = pasteTexture.height;
 
-    int sourceWidth = sourceTexture.width;
-    int sourceHeight = sourceTexture.height;
+        // Resize the source texture, if necessary;
+        if (sourceWidth != w || sourceHeight != h){
+            Texture2D resizedTexture = new Texture2D(w, h);
+            Color32[] resizedPixels = new Color32[w * h];
 
-    // Resize the source texture if necessary
-    if (sourceWidth != w || sourceHeight != h)
-    {
-        Texture2D resizedTexture = new Texture2D(w, h);
-        Color32[] resizedPixels = new Color32[w * h];
-
-        for (int i = 0; i < h; i++)
-        {
-            for (int j = 0; j < w; j++)
-            {
-                int sourceX = Mathf.RoundToInt(j * (sourceWidth / (float)w));
-                int sourceY = Mathf.RoundToInt(i * (sourceHeight / (float)h));
-                resizedPixels[i * w + j] = sourceTexture.GetPixel(sourceX, sourceY);
+            //Round the pixels of the image to fit it into the new texture;
+            for (int i = 0; i < h; i++){
+                for (int j = 0; j < w; j++){
+                    int sourceX = Mathf.RoundToInt(j * (sourceWidth / (float)w));
+                    int sourceY = Mathf.RoundToInt(i * (sourceHeight / (float)h));
+                    resizedPixels[i * w + j] = pasteTexture.GetPixel(sourceX, sourceY);
+                }
             }
+
+            resizedTexture.SetPixels32(resizedPixels);
+            resizedTexture.Apply();
+
+            // Copy the resized texture to the target texture
+            Graphics.CopyTexture(resizedTexture, 0, 0, 0, 0, w, h, skeletonTexture, 0, 0, x, y);
+        }
+        else{
+            // If not, just paste it;
+            Graphics.CopyTexture(pasteTexture, 0, 0, 0, 0, sourceWidth, sourceHeight, skeletonTexture, 0, 0, x, y);
         }
 
-        resizedTexture.SetPixels32(resizedPixels);
-        resizedTexture.Apply();
-
-        // Copy the resized texture to the target texture
-        Graphics.CopyTexture(resizedTexture, 0, 0, 0, 0, w, h, skeletonTexture, 0, 0, x, y);
-    }
-    else
-    {
-        // Copy the source texture directly to the target texture
-        Graphics.CopyTexture(sourceTexture, 0, 0, 0, 0, sourceWidth, sourceHeight, skeletonTexture, 0, 0, x, y);
+        skeletonTexture.Apply();
     }
 
-    skeletonTexture.Apply();
-    }
+    //Fill a color32 array with a specific color;
+    public static Color32[] FillArrayColor(Color color, int w, int h) {
+        Color32[] pixels = new Color32[w * h];
+        for(int i = 0; i < pixels.Length; i++) {
+            pixels[i] = color;
+        }
 
-    public static Color[] FillArrayColor(Color color, int w, int h) {
-        return Enumerable.Repeat(color, w * h).ToArray();
+        return pixels;
     }
 }
+
+////////////////////////////////////////////////////////////////////
 
 //Custom Editor;
 [CustomEditor(typeof(CrAPTextureManagement))]
 public class CustomInspector : Editor {
+
+    //Changes values in the inspector, creating a custom inspector;
     public override void OnInspectorGUI() {
         DrawDefaultInspector();
         CrAPTextureManagement manager = (CrAPTextureManagement) target; //Referencing the script;
@@ -195,11 +196,11 @@ public class CustomInspector : Editor {
                 int h = (int)coordinates.w;
 
                 if(GUILayout.Button("Delete " + name)) {
-                    manager.ClearTextureAt(x, y, w, h);
+                    manager.ClearTextureAt(x, y, w, h, manager.newTextureToManage);
                 }
 
                 if(GUILayout.Button("Paste " + name)) {
-                    manager.PasteTexture(limb, manager.temporalTexture);
+                    manager.PasteTexture(limb, manager.newTextureToManage, manager.skeletonToReference);
                 }
             }
         } catch { }
---
+++ Unity Project/Texture Colorizer/Assets/TexturesSO.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "TexturesSO", menuName = "ScriptableObjects/Texture Extractor SO", order = 1)]
+public class TexturesSO : ScriptableObject {
+    public string textureToSearch;
+    [HideInInspector] [SerializeField] private List<Limb> limbs = new List<Limb>();
+    
+    public List<Limb> GetLimbs(){ return limbs; }
+    public void ClearLimbs(){ limbs.Clear(); }
+    public void AddLimb(Limb limb){ limbs.Add(limb); }
+    public Limb CallLimb(string name){ return limbs.Find(limbSearcher => limbSearcher.GetName() == name); }
+    public void SetLimbs(List<Limb> Limbs){ limbs = Limbs; }
+    // This method is used to mark the changes as dirty
+    public void ApplyChanges(){ EditorUtility.SetDirty(this); }
+}
+
+[System.Serializable]
+public class Limb {
+    [SerializeField] private Texture2D texture;
+    private string name;
+    [SerializeField] private Vector4 coordinates;
+    [SerializeField] private Vector2 pivot;
+
+    public Texture2D GetTexture() { return texture; }
+    public void SetTexture(Texture2D _Texture) { texture = _Texture; }
+    public string GetName() { return name; }
+    public void SetName(string Name) { name = Name; }
+    /* 0: X; Where the sprite box starts;
+     * 1: Y; Where the sprite box ends;
+     * 2: Width; Size of the texture;
+     * 3: Height; Size of the texture; */
+    public Vector4 GetCoordinates() { return coordinates; }
+    public void SetCoordinates(Vector4 Coordinates) { coordinates = Coordinates; }
+    public Vector2 GetPivot() { return pivot; }
+    public void SetPivot(Vector2 Pivot) { pivot = Pivot; }
+}
+
+[CustomEditor(typeof(TexturesSO))] // Replace with the name of your ScriptableObject class
+public class SkeletonSOEditor : Editor{
+    public override void OnInspectorGUI(){
+        TexturesSO scriptableObject = (TexturesSO) target;
+        DrawDefaultInspector();
+
+        // Display a label for a property
+        EditorGUILayout.LabelField("Skeleton Scriptable Object Data", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField(scriptableObject.GetLimbs().Count + " Limbs found");
+        EditorGUILayout.HelpBox("Textures must be in the 'Resources' folder at the project's root for the texture compiling script to function properly.", MessageType.Warning);
+        EditorGUILayout.Space();
+
+        //Confirm search and load button;
+        if (GUILayout.Button("Load Limb Data From Texture")){
+            scriptableObject.ClearLimbs();
+            CrAPTextureManagement.LoadSkeletonData(scriptableObject.textureToSearch, scriptableObject);
+            scriptableObject.ApplyChanges();
+        }
+
+        EditorGUILayout.Space();
+
+        int boxHeight = 20;
+
+        try {
+            foreach(Limb limb in scriptableObject.GetLimbs()) {
+                string name = limb.GetName();
+
+                EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+                EditorGUILayout.LabelField(name, EditorStyles.boldLabel);
+
+                //Coordinates;
+                Vector4 coordinates = limb.GetCoordinates();
+                //Sprite boxes;
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Sprite Box Start", GUILayout.Width(100));
+                EditorGUILayout.SelectableLabel(coordinates.x.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+
+                EditorGUILayout.LabelField("Sprite Box End", GUILayout.Width(100));
+                EditorGUILayout.SelectableLabel(coordinates.y.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+                EditorGUILayout.EndHorizontal();
+
+                //Width / Height;
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Width", GUILayout.Width(100));
+                EditorGUILayout.SelectableLabel(coordinates.z.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+
+                EditorGUILayout.LabelField("Height", GUILayout.Width(100));
+                EditorGUILayout.SelectableLabel(coordinates.w.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+                EditorGUILayout.EndHorizontal();
+
+                //Coordinates;
+                Vector2 pivot = limb.GetPivot();
+                //Pivot;
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Pivot X", GUILayout.Width(100));
+                EditorGUILayout.SelectableLabel(pivot.x.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+
+                EditorGUILayout.LabelField("Pivot Y", GUILayout.Width(100));
+                EditorGUILayout.SelectableLabel(pivot.y.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.Space();
+
+                // Display the label and read-only texture field
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel("Texture");
+                Rect objectFieldRect = GUILayoutUtility.GetRect(EditorGUIUtility.fieldWidth * 2, EditorGUIUtility.fieldWidth * 2);
+
+                EditorGUI.BeginDisabledGroup(true);
+                EditorGUI.ObjectField(objectFieldRect, GUIContent.none, limb.GetTexture(), typeof(Texture2D), false);
+                EditorGUI.EndDisabledGroup();
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.EndVertical();
+            }
+        } catch { }
+    }
+}
