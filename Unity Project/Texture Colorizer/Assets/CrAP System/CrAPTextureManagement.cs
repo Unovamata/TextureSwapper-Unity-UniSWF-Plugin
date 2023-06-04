39ce012915651ed8d57eabd3fc9c52ac8744f539
Unovamata <badelgado@uned.cr>
2023-06-04 16:38:23 -0600

    Texture Set Swapping
    • The "CrAPTextureManager" script now has the capability of swapping textures based on an index. So you can pick and choose what textures are the best for a specific scenario.

--- Unity Project/Texture Colorizer/Assets/CrAP System/CrAPTextureManagement.cs
+++ Unity Project/Texture Colorizer/Assets/CrAP System/CrAPTextureManagement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class CrAPTextureManagement : MonoBehaviour{
     //Data;
-    public TexturesSO textures, textureToReference;
+    public TexturesSO textures;
+    public TexturesSO[] textureToReference;
     public SkeletonSO skeleton;
     [HideInInspector] public Texture2D newTextureToManage;
 
@@ -119,6 +120,7 @@ public class CrAPTextureManagement : MonoBehaviour{
 [CustomEditor(typeof(CrAPTextureManagement))]
 public class CustomInspector : Editor {
     int currentRelationIndex = 0;
+    int currentSelectedTextureToReference = 0;
 
     //Changes values in the inspector, creating a custom inspector;
     public override void OnInspectorGUI() {
@@ -134,36 +136,46 @@ public class CustomInspector : Editor {
             GUILayout.BeginVertical(GUI.skin.box);
             SkeletonRelationships relationship = relationships[currentRelationIndex];
 
-            EditorGUILayout.BeginHorizontal();
-            if(GUILayout.Button("<<", GUILayout.Width(40))) {
-                if(currentRelationIndex <= 0) currentRelationIndex = relationships.Count - 1;
-                else currentRelationIndex--;
-            }
+            EditorGUILayout.LabelField("Body Part:");
+            currentRelationIndex = Scrollable(relationship.GetRelationshipName(), 0, 
+                relationships.Count - 1, currentRelationIndex);
 
-            if(GUILayout.Button(relationship.GetRelationshipName())) {
-                //manager.ClearTextureAt(x, y, w, h, manager.newTextureToManage);
-            }
+            EditorGUILayout.LabelField("Texture Set:");
+            TexturesSO selectedTexture = manager.textureToReference[currentSelectedTextureToReference];
+            string selectedName = selectedTexture.name + " (" + currentSelectedTextureToReference + ")";
+            currentSelectedTextureToReference = Scrollable(selectedName, 0, 
+                manager.textureToReference.Length - 1, currentSelectedTextureToReference);
 
-            if(GUILayout.Button(">>", GUILayout.Width(40))) {
-                if(currentRelationIndex >= relationships.Count - 1) currentRelationIndex = 0;
-                else currentRelationIndex++;
-            }
 
-            EditorGUILayout.EndHorizontal();
             EditorGUILayout.Space();
 
-            if(GUILayout.Button("Clear " + relationship.GetRelationshipName() + " Texture Group")) {
+            if(GUILayout.Button("Swap " + relationship.GetRelationshipName() + " Textures")) {
                 foreach(Limb limb in relationship.GetLimbsRelated()) {
                     manager.ClearTextureAt(limb.GetCoordinates(), manager.newTextureToManage);
+                    manager.PasteTexture(limb, manager.newTextureToManage, 
+                        manager.textureToReference[currentSelectedTextureToReference]);
                 }
             }
 
-            if(GUILayout.Button("Paste " + relationship.GetRelationshipName() + " Texture Group")) {
-                foreach(Limb limb in relationship.GetLimbsRelated()) {
-                    manager.PasteTexture(limb, manager.newTextureToManage, manager.textureToReference);
-                }
-            }
             GUILayout.EndVertical();
         } catch { }
+    }
+
+    private int Scrollable(string name, int min, int max, int index) {
+        EditorGUILayout.BeginHorizontal();
+        if(GUILayout.Button("<<", GUILayout.Width(40))) {
+            if(index <= min) index = max;
+            else index--;
+        }
+
+        GUILayout.Button(name);
+
+        if(GUILayout.Button(">>", GUILayout.Width(40))) {
+            if(index >= max) index = min;
+            else index++;
+        }
+        EditorGUILayout.EndHorizontal();
+
+        return index;
     }
 }
