5b58af51d3dc0d9ff5c62ccdc7ebb1af72098aa4
Unovamata <badelgado@uned.cr>
2023-07-03 10:44:35 -0600

    Removed libraries. Added Pillow & new changes
    • Removed OpenCV & Numpy as Pillow gets the job done for what the project needs in terms of image processing.
    • Finished the "generateMasks.py" script that creates a mask from an image based in a specific color.

--- Unity Project/Texture Colorizer/Assets/StreamingAssets/embedded-python/verify.py
+++
@@ -1,5 +0,0 @@
-import sys
-import cv2
-
-pythonExecutable = sys.executable
-print("Python Executable Path:", pythonExecutable)
---
+++ Unity Project/Texture Colorizer/Assets/StreamingAssets/generateMasks.py
@@ -0,0 +1,27 @@
+import os.path
+from PIL import Image, ImageChops
+
+
+path = r'C:\Users\Administrator\Documents\GitHub\LPSO-Revived-UniSWF-Texture-Colorizer\Unity Project\Texture Colorizer\Assets\Resources\Skeletons\Kitty\Kitty1\Ear Left.png'
+
+def CreateMask(image, RGB, threshold, index):
+    mask = Image.new("RGB", image.size, (0, 0, 0))
+
+    for x in range(image.width):
+        for y in range(image.height):
+            pixel = image.getpixel((x, y))
+
+            if all(p <= t and t - p <= threshold for p, t in zip(pixel[:3], RGB)) and pixel[3] > 0:
+                a = int((pixel[3] / 255) * 255)
+                mask.putpixel((x, y), (a, a, a))
+
+    imageFilename = os.path.basename(path)
+    maskSaveRoute = image.filename.replace(imageFilename, "")
+    filenameWithoutType = imageFilename.replace(".png", "");
+
+    mask.save(f"{maskSaveRoute}{filenameWithoutType} Mask [{index}]{'.png'}")
+    return mask
+
+image = Image.open(path)
+mask = CreateMask(image, (207, 207, 207), 42, 0)
+mask.show()
