d66a77af082b367510eafeac747c96f654e86e5f
Unovamata <badelgado@uned.cr>
2023-12-26 15:48:03 -0600

    MultipleTexturesSO Added
    • A new Texture Scriptable Object to manage multiple textures generated by UniSWF.
    • New interface management for the MultipleTexturesSO objects.

---
+++ Unity Project/Texture Colorizer/Assets/CrAP System/MultipleTexturesSO.cs
@@ -0,0 +1,171 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[CreateAssetMenu(fileName = "MultipleTexturesSO", menuName = "ScriptableObjects/Multiple Texture Extractor SO", order = 1)]
+/* TexturesSO helps saving and extracting the limb assets used
+ * by a specific texture. Serializes the data in files for
+ * later reference by the skeleton. */
+public class MultipleTexturesSO : ScriptableObject {
+    [HideInInspector] [SerializeField] private Texture2D texture;
+    [SerializeField] private Texture2D[] texturesToSearch;
+    [SerializeField] private string subGroupRoute = "";
+    [HideInInspector] [SerializeField] private List<Limb> limbs = new List<Limb>();
+    [SerializeField] HashSet<string> hashSet;
+    [HideInInspector] [SerializeField] private string path = "";
+
+    //Making data save consistently;
+    private void OnEnable() {
+        EditorUtility.SetDirty(this);
+    }
+
+    private void OnValidate() {
+        AssetDatabase.SaveAssets();
+    }
+
+    public List<Limb> GetLimbs(){ return limbs; }
+    public void ClearLimbs(){ limbs.Clear(); }
+    public void AddLimb(Limb limb){ limbs.Add(limb); }
+    public Limb CallLimb(string name){ return limbs.Find(limbSearcher => limbSearcher.GetName() == name); }
+    public void SetLimbs(List<Limb> Limbs){ limbs = Limbs; }
+    public Texture2D GetTexture() { return texture; }
+    public void SetTexture(Texture2D Texture) { 
+        texture = Texture;
+    }
+    public Texture2D GetSingleTexture(int index) { return texturesToSearch[index]; }
+    public void SetSingleTexture(int index, Texture2D texture) { texturesToSearch[index] = texture; }
+    public Texture2D[] GetTexturesToSearch() { return texturesToSearch; }
+    /*public void SetTextureToSearch(int index) { texture = texturesToSearch[index]; }*/
+    public string GetSubGroupRoute(){ return subGroupRoute; }
+    public void SetSubGroupRoute(string SubGroupRoute){ subGroupRoute = SubGroupRoute; }
+    public string GetPath(){ return path; }
+    public void SetPath(){
+        if(texturesToSearch.Length <= 0) return; 
+        
+        path = AssetDatabase.GetAssetPath(texturesToSearch[0]); 
+    }
+
+
+    //***********************************
+
+    //System Management;
+    public Texture2D LoadTextureData(string route, MultipleTexturesSO textures, bool saveAsPng) {
+        //Sprite has to be located in the "Resources" folder;
+        Sprite[] sprites = Resources.LoadAll<Sprite>(FormatRoute(route));
+        string path = CreateFolder(textures.name, textures.GetSubGroupRoute());
+        
+        //Extracting limb data;
+        foreach(Sprite sprite in sprites) {
+            //Formatting the limb;
+            Limb limb = new Limb();
+            limb.SetName(sprite.name);
+
+            Rect rect = sprite.rect;
+            int x = (int) rect.x, y = (int) rect.y,
+                w = (int) rect.width, h = (int) rect.height;
+
+            limb.SetCoordinates(new Vector4(x, y, w, h));
+            /* 0: X; Where the sprite box starts;
+             * 1: Y; Where the sprite box ends;
+             * 2: Width; Size of the texture;
+             * 3: Height; Size of the texture; */
+
+            //Sprite center;
+            limb.SetPivot(new Vector2(sprite.pivot.x / w, sprite.pivot.y / h));
+            //Generating the textures & mapping it to a transparent color;
+            Texture2D texture = Utils.CreateTransparent2DTexture(w, h);
+
+            //Mapping the limb to the texture;
+            Color[] pixels = sprite.texture.GetPixels(x, y, w, h);
+            texture.SetPixels(pixels);
+            
+            texture.Apply();
+
+            // Saving the texture as an asset for later reference;
+            string texturePath = path + "/" + sprite.name; // Specify the desired path and filename
+            string assetPath = texturePath + ".asset";
+            
+            if(saveAsPng) SaveTexture(texturePath, texture, sprite, limb, saveAsPng);
+            AssetDatabase.CreateAsset(texture, assetPath);
+
+            //Saving the data;
+            limb.SetTexture(AssetDatabase.LoadAssetAtPath<Texture2D>(assetPath));
+            textures.AddLimb(limb);
+        }
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+        return Resources.Load<Texture2D>(route);
+    }
+
+    private void SaveTexture(string route, Texture2D texture, Sprite sprite, Limb limb, bool saveAsPng) {
+        route += ".png";
+        Texture2D png = Utils.CreateTransparent2DTexture(texture.width, texture.height);
+
+        //Mapping the limb to the texture;
+        Rect rect = sprite.rect;
+        Color[] pixels = sprite.texture.GetPixels((int) rect.x, (int) rect.y, 
+                                                  (int) rect.width, (int) rect.height);
+        png.SetPixels(pixels);
+        png.Apply();
+
+        byte[] bytes = png.EncodeToPNG();
+        File.Create(route).Dispose();
+        File.WriteAllBytes(route, bytes);
+
+        if(saveAsPng) limb.SetMaskRouteReference(route);
+    }
+
+    //Create a readable route by the asset database;
+    public string FormatRoute(string route) {
+        //Removing the first part of the route;
+        route = route.Replace("Assets/Resources/", "");
+
+        //Removing the .extension part of the route;
+        route = route.Substring(0, route.IndexOf("."));
+
+        return route;
+    }
+
+    //Create a folder to hold assets;
+    public string CreateFolder(string textureName, string groupRoute){
+        string baseRoute = "Assets/Resources/Skeletons";
+        string[] groupSplit;
+        string folderPath = "";
+
+        // If the group doesn't have a route
+        if (groupRoute.Equals("")){
+            groupSplit = textureName.Split("/");
+        }
+        else{
+            groupSplit = (groupRoute + "/" + textureName).Split("/");
+        }
+
+        for(int i = 0; i < groupSplit.Length; i++) {
+            string folderName = groupSplit[i];
+            folderPath += "/" + folderName;
+            
+
+            // If the folder has a valid path and name, create it
+            string relativeFolderPath = baseRoute + folderPath;
+            string folderCreationRoute = baseRoute + folderPath.Substring(0, folderPath.LastIndexOf('/'));
+            string folderToReplace = folderCreationRoute + "/" + textureName;
+
+            if (AssetDatabase.IsValidFolder(relativeFolderPath) && i == groupSplit.Length - 1) {
+                AssetDatabase.DeleteAsset(folderToReplace);
+            }
+
+            if (!AssetDatabase.IsValidFolder(relativeFolderPath)){
+                AssetDatabase.CreateFolder(folderCreationRoute, folderName);
+                Debug.Log("Folder Created: " + folderName);
+            }
+        }
+
+        return baseRoute + folderPath;
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////
---
+++ Unity Project/Texture Colorizer/Assets/CrAP System/MultipleTexturesSOEditor1.cs
@@ -0,0 +1,311 @@
+using Python.Runtime;
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UIElements;
+
+////////////////////////////////////////////////////////////////////
+
+//Custom Editor;
+[CustomEditor(typeof(MultipleTexturesSO))] // Replace with the name of your ScriptableObject class
+public class MultipleTexturesSOEditor : Editor{
+    public bool saveTextureAsPng = false;
+
+    public override void OnInspectorGUI(){
+        MultipleTexturesSO textures = (MultipleTexturesSO) target;
+        base.OnInspectorGUI();
+        string subGroupName = textures.GetSubGroupRoute();
+        textures.SetPath();
+
+        //Naming a subgroup of folders if needed;
+        if (!subGroupName.Equals("") && textures.GetLimbs().Count == 0) {
+            EditorGUILayout.HelpBox("Specify this field ONLY IF the texture belongs to a subgroup of a main group.", MessageType.Info);
+        }
+
+        //Removing the "/" character as it can generate conflicts between routes;
+        if (subGroupName.EndsWith("/")){
+            textures.SetSubGroupRoute(subGroupName.Substring(0, subGroupName.Length - 1));
+        }
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Save Texture Assets + PNGs?", GUILayout.Width(250));
+        saveTextureAsPng = EditorGUILayout.Toggle(saveTextureAsPng);
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+
+        // Display a label for a property
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Skeleton Scriptable Object Data:", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField(textures.GetLimbs().Count + " Limbs found");
+        EditorGUILayout.EndHorizontal();
+        if(textures.GetLimbs().Count == 0)
+            EditorGUILayout.HelpBox("- Ensure the texture is located anywhere within the 'Resources' folder in the project's root.\n- The texture name cannot contain '.' characters.", MessageType.Warning);
+        EditorGUILayout.Space();
+
+        //Confirm search and load button;
+        if (GUILayout.Button("Load Limb Data From Texture")){
+            textures.ClearLimbs();
+            textures.LoadTextureData(textures.GetPath(), textures, saveTextureAsPng);
+        }
+
+        //Clearing padding artifacts;
+        if (GUILayout.Button("Clean Texture Padding Artifacts")){
+            Texture2D source = ConvertDXT5ToRGBA32(textures.GetTexture());
+            
+            foreach(Limb limb in textures.GetLimbs()) {
+                int padding = (int) Prefs.padding / 2;
+                int x = limb.GetX(), y = limb.GetY(), w = limb.GetWidth(), h = limb.GetHeight();
+
+                Utils.ClearTextureAt(new Vector4(x, y + h - padding, w, padding), source);
+                Utils.ClearTextureAt(new Vector4(x + w - padding, y, padding, h), source);
+                Utils.ClearTextureAt(new Vector4(x, y, w, padding), source);
+                Utils.ClearTextureAt(new Vector4(x, y, padding, h), source);
+            }
+
+            Utils.SaveTexture(source, textures.GetPath());
+            textures.SetTexture(source);
+        }
+
+        if(GUILayout.Button("Generate Texture Masks")) {
+            PythonEngine.Initialize();
+
+            using (Py.GIL()){
+                Utils.PythonGoToFolder();
+
+                dynamic sys = Py.Import("sys");
+
+                try {
+                    dynamic script = Py.Import("generateMasks");
+                    string imagePath = @"C:\Users\Administrator\Documents\GitHub\LPSO-Revived-UniSWF-Texture-Colorizer\Unity Project\Texture Colorizer\Assets\Resources\Skeletons\Kitty\Kitty1\Ear Left.png";
+
+                    dynamic PIL = Py.Import("PIL.Image");
+                    dynamic image = PIL.open(imagePath);
+
+                    dynamic mask = script.CreateMask(image, new object[] { 207, 207, 207 }, 42, 0);
+                } catch (PythonException ex){ /*Utils.PythonErrorHandling(ex);*/ }
+            }
+
+            PythonEngine.Shutdown();
+        }
+    
+        EditorGUILayout.Space();
+
+        GUILayout.Label("Limb List:", EditorStyles.boldLabel);
+
+        bool showMaskColorMetadata = (int) Prefs.showMaskTextureMetadata == 1;
+        if (showMaskColorMetadata) {
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.LabelField("Data in Clipboard:");
+            ColorAndThresholdSetter(clipboardColor, clipboardThreshold);
+            EditorGUI.EndDisabledGroup();
+            EditorGUILayout.Space();
+        }
+
+        try {
+            foreach(Limb limb in textures.GetLimbs()) {
+                LoadLimbInGUI(limb, 2, true);
+            }
+        } catch { }
+    }
+
+    public static Color clipboardColor = new Color();
+    public static int clipboardThreshold = 0;
+
+    public Texture2D ConvertDXT5ToRGBA32(Texture2D dxt5Texture){
+        Texture2D rgba32Texture = new Texture2D(dxt5Texture.width, dxt5Texture.height, TextureFormat.RGBA32, false);
+        rgba32Texture.SetPixels32(dxt5Texture.GetPixels32());
+        rgba32Texture.Apply();
+        return rgba32Texture;
+    }
+
+    //Loads all the needed data in the GUI;
+    public static void LoadLimbInGUI(Limb limb, float rectSize, bool showMetadata) {
+        int boxHeight = 20;
+        string name = limb.GetName();
+
+        EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+        EditorGUILayout.LabelField(name, EditorStyles.boldLabel);
+
+        // Display the label and read-only texture field
+        ShowTextureInField(limb.GetTexture(), "Main Texture", rectSize);
+
+        if (!showMetadata) {
+            EditorGUILayout.EndVertical();
+            return;
+        }
+
+        bool showDetailedMetadata = (int)Prefs.showDetailedMetadata == 1;
+
+        if (showDetailedMetadata) {
+            //Coordinates;
+            Vector4 coordinates = limb.GetCoordinates();
+
+            //Sprite boxes;
+            EditorGUILayout.Space();
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Sprite Box Start", GUILayout.Width(100));
+            EditorGUILayout.SelectableLabel(coordinates.x.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+
+            EditorGUILayout.LabelField("Sprite Box End", GUILayout.Width(100));
+            EditorGUILayout.SelectableLabel(coordinates.y.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+            EditorGUILayout.EndHorizontal();
+
+            //Width / Height;
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Width", GUILayout.Width(100));
+            EditorGUILayout.SelectableLabel(coordinates.z.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+
+            EditorGUILayout.LabelField("Height", GUILayout.Width(100));
+            EditorGUILayout.SelectableLabel(coordinates.w.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+            EditorGUILayout.EndHorizontal();
+
+            //Coordinates;
+            Vector2 pivot = limb.GetPivot();
+            //Pivot;
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Pivot X", GUILayout.Width(100));
+            EditorGUILayout.SelectableLabel(pivot.x.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+
+            EditorGUILayout.LabelField("Pivot Y", GUILayout.Width(100));
+            EditorGUILayout.SelectableLabel(pivot.y.ToString(), EditorStyles.textField, GUILayout.Height(boxHeight));
+            EditorGUILayout.EndHorizontal();
+            EditorGUI.EndDisabledGroup();
+            EditorGUILayout.Space();
+        }
+
+        //Texture Mask;
+        //ShowTextureInField(limb.GetMaskReference(), "Mask Texture Reference", rectSize);
+
+        //Mask Colors;
+        ShowMaskColorManagement(limb);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.EndVertical();
+    }
+
+    public static void ShowTextureInField(Texture2D texture, string textureName, float rectSize){
+        bool showMaskTextureMetadata = (int) Prefs.showMaskTextureMetadata == 1;
+        if(!showMaskTextureMetadata) return;
+
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+        // Display the label and read-only texture field
+        EditorGUILayout.PrefixLabel(textureName);
+        Rect objectFieldRect = GUILayoutUtility.GetRect(EditorGUIUtility.fieldWidth * rectSize, EditorGUIUtility.fieldWidth * rectSize);
+
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUI.ObjectField(objectFieldRect, GUIContent.none, texture, typeof(Texture2D), false);
+        EditorGUI.EndDisabledGroup();
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+    }
+
+
+    public static float rectangleSize = 20f;
+    public static float colorRectangleSize = 60f;
+    public static int thresholdInputValue = 0;
+    public static Color colorInputValue = new Color();
+    public static List<LimbColor> clipboardColorList = new List<LimbColor>();
+
+    public static void ShowMaskColorManagement(Limb limb) {
+        //If the GUI should not check for color metadata;
+        bool showMaskColorMetadata = (int) Prefs.showMaskTextureMetadata == 1;
+        if (!showMaskColorMetadata) return;
+
+        //Folder;
+        limb.SetColorsFolder(EditorGUILayout.Foldout(limb.GetColorsFolder(), "Mask Color Management"));
+        if (!limb.GetColorsFolder()) return;
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Pass Entire Texture as a Mask?");
+        limb.SetPassLimbAsMask(EditorGUILayout.Toggle(limb.GetPassLimbAsMask()));
+        EditorGUILayout.EndHorizontal();
+
+        if (limb.GetPassLimbAsMask()) return;
+        EditorGUILayout.HelpBox("Assign colors to the list below to indicate different parts of the texture. Python will use these colors to separate the masks accordingly.", MessageType.Info);
+
+        //Mask Colors;
+        EditorGUILayout.Space();
+        List<LimbColor> colorList = limb.GetMaskColors();
+
+        if(colorList.Count > 0) EditorGUILayout.LabelField("Color & Threshold");
+
+        for(int i = 0; i < colorList.Count; i++) {
+            LimbColor limbColor = limb.GetMaskColor(i);
+            EditorGUILayout.BeginHorizontal();
+
+            limbColor.SetColor(EditorGUILayout.ColorField(limbColor.GetColor()));
+            limbColor.SetThreshold(EditorGUILayout.IntField(limbColor.GetThreshold()));
+
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Remove", GUILayout.Width(colorRectangleSize))) {
+                limb.RemoveAtMaskColor(i);
+            }
+
+            //Copy color;
+            if (GUILayout.Button("Copy", GUILayout.Width(colorRectangleSize))) {
+                clipboardColor = limbColor.GetColor();
+                clipboardThreshold = limbColor.GetThreshold();
+            }
+
+            //Paste color;
+            if (GUILayout.Button("Paste", GUILayout.Width(colorRectangleSize))) {
+                bool hasColor = colorList.Any(x => x.GetColor().Equals(clipboardColor));
+
+                if(!hasColor) {
+                    LimbColor referenceLimbColor = colorList.ElementAt(i);
+                    referenceLimbColor.SetColor(clipboardColor);
+                    referenceLimbColor.SetThreshold(clipboardThreshold);
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+        
+        //Color picker and threshold setter;
+        EditorGUILayout.LabelField("Input Color & Threshold");
+        EditorGUILayout.BeginHorizontal();
+        colorInputValue = EditorGUILayout.ColorField(new Color(colorInputValue.r, colorInputValue.g, colorInputValue.b, 1));
+        thresholdInputValue = EditorGUILayout.IntField(thresholdInputValue);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        
+        if (GUILayout.Button("Add")) {
+            bool hasColor = colorList.Any(x => x.GetColor().Equals(colorInputValue));
+            if(!hasColor) {
+                LimbColor newLimbColor = new LimbColor(colorInputValue, thresholdInputValue);
+                limb.AddMaskColor(newLimbColor);
+                colorInputValue = new Color(0, 0, 0, 1);
+            }
+        }
+
+        if (GUILayout.Button("Copy All")) {
+            clipboardColorList = colorList;
+        }
+
+        if (GUILayout.Button("Paste New")) {
+            limb.SetMaskColors(clipboardColorList);
+        }
+
+        if (GUILayout.Button("Paste Ref")) {
+            limb.SetMaskColors(clipboardColorList);
+        }
+
+        if (GUILayout.Button("Clear")) {
+            limb.ClearMaskColors();
+        }
+
+        EditorGUILayout.EndHorizontal();
+    }
+
+    public static void ColorAndThresholdSetter(Color color, int threshold) {
+        EditorGUILayout.BeginHorizontal();
+        color = EditorGUILayout.ColorField(color);
+        threshold = EditorGUILayout.IntField(threshold);
+        EditorGUILayout.EndHorizontal();
+    }
+}
